edbf81fd90a4a0ef2f5024cc14bd535e56557ccd
rositsa.krasteva <rositsa.krasteva@accenture.com>
2018-08-22 12:57:49 +0200

    Prinzipien 5-8

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        
+		  <ul>
+		  <li > <b> Prinzip 5: </b> Build projects around motivated individuals. 
+Give them the environment and support they need, 
+and trust them to get the job done. <br></br>
+<li>  Individuals and interactions over processes and tools</li> 
+  </li> 
+  <li>  <b> Prinzip 6:  </b>The most efficient and effective method of 
+conveying information to and within a development 
+team is face-to-face conversation.<br></br>
+<li> Individuals and interactions over processes and tools</li> 
+  </li> 
+  <li>  <b> Prinzip 7: </b> Working software is the primary measure of progress.
+<li> Working software over comprehensive documentation</li> 
+  </li>
+  <li>  <b> Prinzip 8: </b> Agile processes promote sustainable development. 
+The sponsors, developers, and users should be able 
+to maintain a constant pace indefinitely.<br></br>
+<li>Customer collaboration over contract negotiation </li>
+  </li> 
+
+		  </ul>
         </p>
       </div>
     );
