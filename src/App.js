47f4cfd73300203b62f5a428f65048eef78d6fc7
Melatyar <mohammad.j.melatyar@accenture.com>
2018-08-22 13:05:41 +0200

    9-12

--- src/App.js
+++ src/App.js
@@ -7,12 +7,40 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        
+          <h1 className="App-title"> Principles 9 - 12  and their corresponding values of Agile Manifesto</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+        <li>Continuous attention to technical excellence 
+and good design enhances agility.<br></br>
+Value: Working software over comprehensive documentation<br></br>
+Comments: Technical excellence allows for agility
+</li>
+
+
+ <li>Simplicity--the art of maximizing the amount 
+of work not done--is essential.<br></br>
+Values: 
+Customer collaboration over contract negotiation
+Responding to change over following a plan <br></br>
+Comments: Being clear of what needs to be done gives clarity and makes thinking about he problem more easier
+</li>
+
+<li>The best architectures, requirements, and designs 
+emerge from self-organizing teams.<br></br>
+Values: 
+Individuals and interactions over processes and tools<br></br>
+Comments: Self-organizing teams can interact better
+</li>
+
+<li>At regular intervals, the team reflects on how 
+to become more effective, then tunes and adjusts 
+its behavior accordingly.<br></br>
+Value: Responding to change over following a <br></br>
+Comments: Reflection results in behavioural change
+</li>
+
+        </ul>
       </div>
     );
   }
