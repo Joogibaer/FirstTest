28878ed638851f3ead1542a5806e0a050ec4ed69
Melatyar <mohammad.j.melatyar@accenture.com>
2018-08-23 12:43:39 +0200

    sprint Backlog

---
+++ src/Sbacklog.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import devteam from './devteam.jpg';
+import sprintblog from './sprintblog.jpg'
+
+class Sbacklog extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      
+      <h2> Sprint Backlog </h2>
+      <img src = {sprintblog} className = "sprintblog" alt ="pic" />
+      </div>
+    );
+  }
+}
+
+export default Sbacklog;
